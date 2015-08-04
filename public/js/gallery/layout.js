381126057b97fa86829b02834817ba7076e01515
Eric Baum <nanopy@nanopy.com>
2015-08-04 14:28:08 -0400

    Should improve the perceived load time for Masonry layout.

--- public/js/gallery/layout.js
+++ public/js/gallery/layout.js
@@ -1,29 +1,19 @@
 $(function() {
-    $(window).load(function() {
-        // initialize Isotope
-        var $container = $('#iso').isotope({
-            masonry: {
-                gutter: 20
-            }
-        });
+    // initialize Isotope
+    var $container = $('#iso').isotope({
+        masonry: {
+            gutter: 20
+        }
+    });
 
-        $container.isotope('option', { animationEngine: 'css', 'transitionDuration': 0 }).addClass('transitions-disabled');
+    $container.isotope('option', { animationEngine: 'css', 'transitionDuration': 0 }).addClass('transitions-disabled');
 
-        // layout Isotope again after all images have loaded
-        $container.imagesLoaded(function () {
-            if ($(window).width() <= 1024)
-            {
-                setTimeout(function() {
-                    $container.isotope('layout');
-                    setTimeout(function() {
-                        $container.isotope('layout');
-                    }, 10);
-                }, 10);
-            }
-            else
-            {
-                $container.isotope('layout');
-            }
+    // layout Isotope again after all images have loaded
+    $container.imagesLoaded(function () {
+        $container.isotope('layout');
+
+        $(window).load(function() {
+            $container.isotope('layout');
         });
     });
 
